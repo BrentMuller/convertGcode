307ccbf41fc4906602fc8d46dc6e19e61690a941
brentus <brent@darkstar.music_slack.org>
2023-07-02 21:03:54 -0600

    Parses a simple G code file

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,15 @@ module Main where
 
 import Data.Attoparsec.ByteString
 import qualified Data.ByteString as B
-import Data.Attoparsec.ByteString.Char8
+import Data.Attoparsec.ByteString.Char8 as Atto8
+import Data.ByteString.Char8 as BC
 -------------------------------------------------------------------------------
 main :: IO ()
 main = do
     file <- B.readFile "test.tap" 
     B.putStr file
     doit theParser file
---    let result = parse theParser file
- --   print $ "result: " ++ show result
+
 -------------------------------------------------------------------------------
 --parse :: Parser a -> ByteString -> Result a
 -- Partial (i -> IResult i r)	
@@ -34,24 +34,54 @@ doit prsr input = do
     return ()
 
 -------------------------------------------------------------------------------
---theParser::Parser B.ByteString
 theParser::Parser [B.ByteString]
 theParser = do
---    str<- manyTill anyWord8 endOfInput
---    str<- manyTill line endOfInput
---    manyTill anyWord8 endOfInput
+    string $ BC.singleton '%'
+    endOfLine
     str <- many1 line
-  --  endOfInput
---    return $ B.concat str
-    takeByteString
+    string $ BC.singleton '%'
+    endOfLine
     return str
---    return $ B.pack str
-
 -------------------------------------------------------------------------------
 line::Parser B.ByteString
 line = do
-    --chars <- many1 anyWord8 
-    chars <- manyTill anyWord8 endOfLine
---    endOfLine
-    return $ B.pack chars
+    words <- choice [comment,manyTill gWord endOfLine ]
+    return $ B.concat words
+-------------------------------------------------------------------------------
+gWord::Parser B.ByteString
+gWord = do
+   a <-letter_ascii 
+   --b <- choice [integerWord,floatWord]
+--   b <- choice [integerWord,floatWord,endOfLineByteStr]
+   b <-wordRemainder
+--   b <- Atto8.takeWhile (Atto8.inClass "-0123456789+.")
+   Atto8.takeWhile (\x->x==' ')
+   return $ singleton a  `B.append` (BC.pack.show) b `B.append` BC.pack "test"
+-------------------------------------------------------------------------------
+comment::Parser [B.ByteString]
+comment = do
+    a<-string $ BC.singleton '(' 
+    b<-manyTill anyChar  $ string $ BC.singleton ')' 
+    endOfLine
+    return $ [BC.pack "comment...." ] ++ [BC.pack b]
+-------------------------------------------------------------------------------
+wordRemainder::Parser B.ByteString
+wordRemainder = do
+    a <- Atto8.takeWhile (Atto8.inClass "-0123456789+.")
+    return a
+-------------------------------------------------------------------------------
+-- {-
+integerWord::Parser B.ByteString
+integerWord = do
+   b <-decimal::Parser Int
+   return $ (BC.pack.show) b
+-------------------------------------------------------------------------------
+floatWord::Parser B.ByteString
+floatWord = do
+    c <-double
+    return $ BC.pack $ show c
+-- -}
+-------------------------------------------------------------------------------
+endOfLineByteStr::Parser ByteString
+endOfLineByteStr = return empty
 -------------------------------------------------------------------------------
