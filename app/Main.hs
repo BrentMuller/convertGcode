6c918698be6c28cd081c6210b6e10de336e1686d
brentus <brent@darkstar.music_slack.org>
2023-07-04 15:19:16 -0600

    ready to test- changed to Attoparsec Text from ByteString.

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,28 @@
 module Main where
 
-import Data.Attoparsec.ByteString
-import qualified Data.ByteString as B
-import Data.Attoparsec.ByteString.Char8 as Atto8
-import Data.ByteString.Char8 as BC
+import Data.Attoparsec.Text.Lazy  as AttoT
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.IO as TIO
+import System.FilePath.Posix (dropExtension)
+import System.Environment
+import System.Exit
+import GHC.List
 -------------------------------------------------------------------------------
 main :: IO ()
 main = do
-    file <- B.readFile "test.tap" 
-    B.putStr file
-    doit theParser file
+    args <- getArgs
+    fileName <- case uncons args of
+            Just (a,_) -> return a
+            Nothing -> do
+               print "no arguments given- exiting.."
+               exitWith $ ExitFailure 1
+    doit fileName theParser 
 
 -------------------------------------------------------------------------------
---parse :: Parser a -> ByteString -> Result a
--- Partial (i -> IResult i r)	
-doit::(Show a)=>Parser a -> B.ByteString -> IO () 
-doit prsr input = do
+doit::FilePath -> Parser [TL.Text] ->  IO () 
+doit filePath prsr = do
+    input <- TIO.readFile filePath
     let result = parse prsr input
     case result of
         Fail unconsumed contexts message -> do
@@ -24,64 +31,54 @@ doit prsr input = do
             print $ "contexts: " ++ show contexts
             print $ "message: " ++ show message
             return ()
-        Partial p -> do
-            let i = p B.empty -- ::IResult B.ByteString B.ByteString
-            print $ "partial ; "  ++ (show i)
-            return ()
-        Done _ _-> do
+        Done t r-> do
+            TIO.writeFile ((dropExtension filePath) ++ ".out.tap") $ TL.concat r
             print "Success!"
-    print $ "result: " ++ show result
     return ()
-
 -------------------------------------------------------------------------------
-theParser::Parser [B.ByteString]
+theParser::Parser [TL.Text]
 theParser = do
-    string $ BC.singleton '%'
+    string $ T.singleton '%'
     endOfLine
     str <- many1 line
-    string $ BC.singleton '%'
+    string $ T.singleton '%'
     endOfLine
-    return str
+    return $ [TL.pack "%\n"] ++  str ++ [TL.pack "%"]
 -------------------------------------------------------------------------------
-line::Parser B.ByteString
+line::Parser TL.Text
 line = do
     words <- choice [comment,manyTill gWord endOfLine ]
-    return $ B.concat words
+    return $ TL.concat words `TL.append` TL.pack "\n"
 -------------------------------------------------------------------------------
-gWord::Parser B.ByteString
+gWord::Parser TL.Text
 gWord = do
-   a <-letter_ascii 
-   --b <- choice [integerWord,floatWord]
---   b <- choice [integerWord,floatWord,endOfLineByteStr]
-   b <-wordRemainder
---   b <- Atto8.takeWhile (Atto8.inClass "-0123456789+.")
-   Atto8.takeWhile (\x->x==' ')
-   return $ singleton a  `B.append` (BC.pack.show) b `B.append` BC.pack "test"
+   a <-letter
+   b <-choice [parseNoLeadingZero,parseNoLeadingZeroMinus,wordRemainder]
+   AttoT.takeWhile (\x->x==' ')
+   return $ TL.singleton a  `TL.append`  (TL.pack.T.unpack) b `TL.append` TL.pack " "
 -------------------------------------------------------------------------------
-comment::Parser [B.ByteString]
+comment::Parser [TL.Text]
 comment = do
-    a<-string $ BC.singleton '(' 
-    b<-manyTill anyChar  $ string $ BC.singleton ')' 
+    a<-string $ T.singleton '(' 
+    b<-manyTill anyChar  $ string $ T.singleton ')' 
     endOfLine
-    return $ [BC.pack "comment...." ] ++ [BC.pack b]
+    return $ [TL.pack "(" ] ++ [TL.pack b] ++ [TL.pack ") " ] 
 -------------------------------------------------------------------------------
-wordRemainder::Parser B.ByteString
+wordRemainder::Parser T.Text
 wordRemainder = do
-    a <- Atto8.takeWhile (Atto8.inClass "-0123456789+.")
+    a <- AttoT.takeWhile (inClass "-0123456789+.")
     return a
 -------------------------------------------------------------------------------
--- {-
-integerWord::Parser B.ByteString
-integerWord = do
-   b <-decimal::Parser Int
-   return $ (BC.pack.show) b
+parseNoLeadingZero::Parser T.Text
+parseNoLeadingZero = do
+    a<-char '.' 
+    b<-wordRemainder
+    return  $ T.pack "0." `T.append` b
 -------------------------------------------------------------------------------
-floatWord::Parser B.ByteString
-floatWord = do
-    c <-double
-    return $ BC.pack $ show c
--- -}
--------------------------------------------------------------------------------
-endOfLineByteStr::Parser ByteString
-endOfLineByteStr = return empty
+parseNoLeadingZeroMinus::Parser T.Text
+parseNoLeadingZeroMinus = do
+    a<-char '-' 
+    b<-char '.'
+    c<-wordRemainder
+    return  $ T.pack "-0." `T.append`  c
 -------------------------------------------------------------------------------
